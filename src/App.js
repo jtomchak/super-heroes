d7bf29c2e1ad1b74d81312588a42aac739ac454c
Jesse Tomchak <jtomchak@gmail.com>
2017-11-06 13:54:25 -0700

    first super hero starter

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+//Source Control, you know like git? Maybe ?
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Super Heroes</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
