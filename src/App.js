87903b264a83320202116bb404e4b842925f7b2d
Jesse Tomchak <jtomchak@gmail.com>
2017-11-06 14:31:16 -0700

    Heroes List

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
-//Source Control, you know like git? Maybe ?
+import SuperHeroes from "./superheroes";
 
 class App extends Component {
+  state = {
+    title: "Super Heroes",
+    heroes: SuperHeroes.HEROES
+  };
+
   render() {
+    const heroesList = this.state.heroes.map(function(hero, index) {
+      return (
+        <li>
+          <span>{index}:</span> {hero.superhero}
+        </li>
+      );
+    });
+    console.log(heroesList);
     return (
       <div>
-        <h1>Super Heroes</h1>
+        <h1>{this.state.title}</h1>
+        <ul>{heroesList}</ul>
       </div>
     );
   }
