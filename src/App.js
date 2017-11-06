acafef3523b8c3eddb261621ed94210567fcc529
Jesse Tomchak <jtomchak@gmail.com>
2017-11-06 15:49:59 -0700

    on click events

--- src/App.js
+++ src/App.js
@@ -5,22 +5,38 @@ import SuperHeroes from "./superheroes";
 class App extends Component {
   state = {
     title: "Super Heroes",
-    heroes: SuperHeroes.HEROES
+    heroes: SuperHeroes.HEROES,
+    selectedHero: {}
+  };
+
+  handleSelectedHero = hero => {
+    // this.state.selectedHero = hero;
+    this.setState({
+      selectedHero: hero
+    });
   };
 
   render() {
-    const heroesList = this.state.heroes.map(function(hero, index) {
+    const heroesList = this.state.heroes.map(hero => {
       return (
-        <li>
+        <li key={hero.id} onClick={() => this.handleSelectedHero(hero)}>
           <span className="badge">{hero.id}:</span> {hero.superhero}
         </li>
       );
     });
-    console.log(heroesList);
     return (
       <div className="App">
         <h1>{this.state.title}</h1>
         <ul className="heroes">{heroesList}</ul>
+        <div>
+          <label>ID: </label>
+          {this.state.selectedHero.id}
+        </div>
+        <form>
+          <label>Hero Name:</label>
+          <input type="text" value={this.state.selectedHero.superhero} />
+          <input className="button" type="submit" value="submit" />
+        </form>
       </div>
     );
   }
